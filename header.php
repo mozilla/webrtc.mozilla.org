76fcc803daad8970aebd66ced6681af4d464d1f4
John Yoder <johngilesyoder@gmail.com>
2016-05-26 21:47:52 -0600

    Homepage hero completely responsive

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
         <section class="home-hero">
           <div class="container">
             <div class="row">
-              <div class="col-md-4 col-md-push-8">  
+              <div class="col-md-4 col-md-push-8 illustration-wrapper">  
                 <img class="illustration" src="<?php echo get_template_directory_uri(); ?>/assets/img/illustration-home.svg">
               </div>
               <div class="col-md-8 col-md-pull-4">
